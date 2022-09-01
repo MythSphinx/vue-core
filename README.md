7bfcdaba50e5ffa80e0b9b1cf1b5aa84099a81eb
MythSphinx <alexjr968@outlook.com>
2022-09-01 00:05:44 -0300

    More tries, but...

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # vue-core
+
+Every tutorial I found of a project including these two frameworks (Vue and .NET Core) had one or two things outdated, unfortunately. I'll be doing two projects for them separately.
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,59 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
+    <header>
+      <b-navbar toggleable="md" type="light" variant="light">
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-navbar-brand to="/">Love Pizza</b-navbar-brand>
+        <b-collapse is-nav id="nav-collapse">
+          <b-navbar-nav>
+            <b-nav-item href="#" @click.prevent="login" v-if="!user"
+              >Login</b-nav-item
+            >
+            <b-nav-item href="#" @click.prevent="logout" v-else
+              >Logout</b-nav-item
+            >
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </header>
+    <main>
+      <div>Love pizza button and clicks counter here</div>
+    </main>
   </div>
 </template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      user: null,
+    };
+  },
+  async created() {
+    await this.refreshUser();
+  },
+  watch: {
+    $route: "onRouteChange",
+  },
+  methods: {
+    login() {
+      this.$auth.loginRedirect();
+    },
+    async onRouteChange() {
+      await this.refreshUser();
+    },
+    async refreshUser() {
+      this.user = await this.$auth.getUser();
+    },
+    async logout() {
+      await this.$auth.logout();
+      await this.refreshUser();
+      this.$router.push("/");
+    },
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -17,7 +64,7 @@
   color: #2c3e50;
 }
 
-nav {
+#nav {
   padding: 30px;
 
   a {
